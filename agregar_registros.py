c04256afb227cd07bfdbc43f89322dff0772aa8a
prramos1@utpl.edu.ec <prramos1@utpl.edu.ec>
2024-05-23 17:06:02 -0500

    mi primera chamba

---
+++ agregar_registros.py
@@ -0,0 +1,38 @@
+from sqlalchemy.orm import sessionmaker
+# se importa la clase(s) del
+# archivo crear_entidades
+from crear_entidades import localesComida, centrosDeportivos
+# se importa el engine
+from base_datos import engine
+
+# Se crea una clase llamada Sessión,
+# desde el generador de clases de SQLAlchemy
+# sessionmaker.
+Session = sessionmaker(bind=engine) # Se usa el engine
+# Importante, se crea un objeto llamado session
+# de tipo Session, que permite: permitir guardar, eliminar,
+# actualizar y generar consultas a la base de datos.
+session = Session()
+
+# se crea un objetos de tipo locales_comida
+comercial1 = localesComida(nombreLocal="Conchitas Asadas", localidad="Quito",    direccion="Av. Pichincha", capacidad=80)
+comercial2 = localesComida(nombreLocal="Rincon del Sabor", localidad="Calderon", direccion="Av. Guayaquil", capacidad=100)
+comercial3 = localesComida(nombreLocal="La Foch",          localidad="Centro",   direccion="Av. Amazonas",  capacidad=120)
+        
+
+# a la espera de un commit
+session.add( comercial1 )     
+session.add( comercial2 )
+session.add( comercial3 )
+
+# se crea un objetos de tipo centros_deportivos
+centro1 = centrosDeportivos(nombreCentro="Concentracion de futbol", localidad="Quito", tamaño="Coliseo", capacidad=300)
+centro2 = centrosDeportivos(nombreCentro="Polideportivo del Sur",   localidad="Quito", tamaño="Estadio", capacidad=500) 
+
+        
+# a la espera de un commit
+session.add( centro1)
+session.add( centro2)
+# se necesita confirmar los cambios que existan en la sessió
+# se usar commit
+session.commit()
---
+++ consultar_registros02.py
@@ -0,0 +1,29 @@
+from sqlalchemy.orm import sessionmaker
+# se importa la clase(s) del
+# archivo crear_entidades
+from crear_entidades import centrosDeportivos
+# se importa el engine
+from base_datos import engine
+
+# Se crea una clase llamada Sessión,
+# desde el generador de clases de SQLAlchemy
+# sessionmaker.
+Session = sessionmaker(bind=engine) # Se usa el engine
+# Importante, se crea un objeto llamado session
+# de tipo Session, que permite: permitir guardar, eliminar,
+# actualizar y generar consultas a la base de datos.
+session = Session()
+
+# Obtener todos los registros de la entidad Autor con una(s) condición.
+# Se hace uso del método query.
+# filter, permite agregrar condiciones a la búsqueda, con base
+# a las propiedades de la entidad
+lista_centros = session.query(centrosDeportivos).filter(centrosDeportivos.capacidad>300)
+# La variable lista_autores, tendrá un listado de objetos de tipo Autor que
+# tengan en la propiedad de nacionalidad el valor: ecuatoriana
+
+
+# se realiza un proceso iterativo para presentar la información
+# de cada objeto.
+for l in lista_centros:
+    print(l)
